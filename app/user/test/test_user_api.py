b006210f8b26f250cac5074d2ec3fd19ea4f344f
Anand Powar <anandpowar@gmail.com>
2021-05-08 14:38:39 +0530

    include name parameter is user test cases

--- app/user/test/test_user_api.py
+++ app/user/test/test_user_api.py
@@ -37,7 +37,8 @@ class PublicUserApiTests(TestCase):
         """Test  creating a user that already exists"""
         payload = {
             'email': 'test@anand.com',
-            'password': 'test@123'
+            'password': 'test@123',
+            'name': 'Test User'
         }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
@@ -47,7 +48,8 @@ class PublicUserApiTests(TestCase):
         """Test the password must be more than 6 characters"""
         payload = {
             'email': 'short@anand.com',
-            'password': '123'
+            'password': '123',
+            'name': 'Test User'
         }
         res = self.client.post(CREATE_USER_URL, payload)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
